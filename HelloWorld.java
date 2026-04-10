c89e949bf399490dbf73021dc75fad03f20edbdb
bhagyabandi90-lang <bhagyabandi90@gmail.com>
2026-04-10 10:58:20 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
   {
     public static void main(String[] args)
                             {
-                            System.out.println("HelloWorld gpt mudhol");
+                            System.out.println("@HelloWorld gpt mudhol");
                               }
   }
